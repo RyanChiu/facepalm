a7ff5c9978580a43a00580a37b9238bfbc044a1b
Ryan Chiu <ryan.chiu@hotmail.com>
2023-03-20 17:38:13 +0800

    This commit contains a halfdone, or let's put it this way, a very beginning that you could see a "hello world" kind of tentative action and a function that could let you active a windows app and then put a "facepalm" on top of it, that's funny, isn't it? And then it will become more and more useful, if there is enough free time...

---
+++ GetLocked/FormAMB.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GetLocked
+{
+    partial class FormAMB
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnClickMe = new System.Windows.Forms.Button();
+            this.labelShowing = new System.Windows.Forms.Label();
+            this.textBoxShowing = new System.Windows.Forms.TextBox();
+            this.listBoxShowing = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnClickMe
+            // 
+            this.BtnClickMe.Location = new System.Drawing.Point(231, 509);
+            this.BtnClickMe.Name = "BtnClickMe";
+            this.BtnClickMe.Size = new System.Drawing.Size(463, 23);
+            this.BtnClickMe.TabIndex = 0;
+            this.BtnClickMe.Text = "ClickMe";
+            this.BtnClickMe.UseVisualStyleBackColor = true;
+            this.BtnClickMe.Click += new System.EventHandler(this.BtnClickMe_Click);
+            // 
+            // labelShowing
+            // 
+            this.labelShowing.AutoSize = true;
+            this.labelShowing.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelShowing.ForeColor = System.Drawing.Color.Salmon;
+            this.labelShowing.Location = new System.Drawing.Point(408, 545);
+            this.labelShowing.Name = "labelShowing";
+            this.labelShowing.Size = new System.Drawing.Size(103, 16);
+            this.labelShowing.TabIndex = 1;
+            this.labelShowing.Text = "Show me sth.";
+            // 
+            // textBoxShowing
+            // 
+            this.textBoxShowing.Location = new System.Drawing.Point(12, 12);
+            this.textBoxShowing.Multiline = true;
+            this.textBoxShowing.Name = "textBoxShowing";
+            this.textBoxShowing.ReadOnly = true;
+            this.textBoxShowing.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxShowing.Size = new System.Drawing.Size(902, 209);
+            this.textBoxShowing.TabIndex = 2;
+            this.textBoxShowing.WordWrap = false;
+            // 
+            // listBoxShowing
+            // 
+            this.listBoxShowing.FormattingEnabled = true;
+            this.listBoxShowing.ItemHeight = 12;
+            this.listBoxShowing.Location = new System.Drawing.Point(12, 227);
+            this.listBoxShowing.MultiColumn = true;
+            this.listBoxShowing.Name = "listBoxShowing";
+            this.listBoxShowing.Size = new System.Drawing.Size(902, 256);
+            this.listBoxShowing.TabIndex = 3;
+            // 
+            // FormAMB
+            // 
+            this.ClientSize = new System.Drawing.Size(926, 579);
+            this.Controls.Add(this.listBoxShowing);
+            this.Controls.Add(this.textBoxShowing);
+            this.Controls.Add(this.labelShowing);
+            this.Controls.Add(this.BtnClickMe);
+            this.Name = "FormAMB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnClickMe;
+        private System.Windows.Forms.Label labelShowing;
+        private System.Windows.Forms.TextBox textBoxShowing;
+        private System.Windows.Forms.ListBox listBoxShowing;
+    }
+}
+
---
+++ GetLocked/FormAMB.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace GetLocked
+{
+    public partial class FormAMB : Form
+    {
+        public FormAMB()
+        {
+            InitializeComponent();
+        }
+
+        private String mainWinTitle = "Signal";
+
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+
+        private void BtnClickMe_Click(object sender, EventArgs e)
+        {
+            labelShowing.Text = "Hello, world!";
+            if (textBoxShowing.Text == string.Empty)
+            {
+                this.listBoxShowing.MouseDoubleClick += new MouseEventHandler(listBoxShowing_MouseDoubleClick);
+                textBoxShowing.Text = "Hello! World!";
+                BtnClickMe.Text = "Show me the system processes!";
+            }
+            else
+            {
+                textBoxShowing.Text = string.Empty;
+                listBoxShowing.Items.Clear();
+                Process[] myProcesses = Process.GetProcesses();
+                int cnt = 0;
+                foreach (Process myProcess in myProcesses)
+                {
+                    if (myProcess.MainWindowTitle.Length > 0)
+                    {
+                        textBoxShowing.Text += "Task name: " + myProcess.MainWindowTitle
+                           + " [Id:" + myProcess.Id
+                           + ", sessionId:" + myProcess.SessionId
+                           + ", procName:" + myProcess.ProcessName
+                           // + ", Container:" + myProcess.Container.Components.ToString()
+                           + "]" + "\r\n";
+
+                        listBoxShowing.Items.Add(myProcess.MainWindowTitle);
+                    }
+                    if (myProcess.MainWindowTitle == mainWinTitle)
+                    {
+                        cnt++;
+                        MessageBox.Show("Hey, [" + mainWinTitle + ", " + myProcess.Id + "," + myProcess.SessionId + "], there you are.(" + cnt + " times)");
+                    }
+                    else
+                    {
+                        // do nothing or ...
+                    }
+                }
+            }
+        }
+
+        void listBoxShowing_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.listBoxShowing.IndexFromPoint(e.Location);
+            if (index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                // to do what you want
+                String mainWinTitle = listBoxShowing.Items[index].ToString();
+                MessageBox.Show(mainWinTitle);
+                Process[] myProcesses = Process.GetProcesses();
+                foreach (Process myProcess in myProcesses)
+                {
+                    if (myProcess.MainWindowTitle == mainWinTitle)
+                    {
+                        SwitchToThisWindow(myProcess.MainWindowHandle, true);
+                        showMeOverU(myProcess.MainWindowHandle);
+                    }
+                }
+            }
+        }
+
+        void showMeOverU(IntPtr handle)
+        {
+            RECT rect = new RECT
+            {
+                Left = 0,
+                Top = 0,
+                Right = 11,
+                Bottom = 11
+            };
+            _ = GetWindowRect(handle, ref rect);
+
+            if (this.TopMost)
+            {
+                this.TopMost = false;
+            } else
+            {
+                this.TopMost = true;
+            }
+            this.Location = new System.Drawing.Point(rect.Left, rect.Top);
+            this.Size = new System.Drawing.Size(rect.Right - rect.Left, rect.Bottom - rect.Top);
+            this.Show();
+        }
+    }
+}
