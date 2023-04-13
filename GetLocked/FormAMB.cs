6b7c4b02f13efaf029c2f8cb1243332f732a37b8
Ryan Chiu <ryan.chiu@hotmail.com>
2023-04-13 21:28:47 +0800

    Write every "activated window"'s title on the console line by using "SetWinEventHook", according to the following article. https://stackoverflow.com/questions/4372055/detect-active-window-changed-using-c-sharp-without-polling

--- GetLocked/FormAMB.cs
+++ GetLocked/FormAMB.cs
@@ -13,11 +13,46 @@ namespace GetLocked
 {
     public partial class FormAMB : Form
     {
+        WinEventDelegate dele = null;
         public FormAMB()
         {
             InitializeComponent();
             Control.CheckForIllegalCrossThreadCalls = false;
             this.listBoxShowing.MouseDoubleClick += new MouseEventHandler(listBoxShowing_MouseDoubleClick);
+
+            dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            // Write every "activated window"'s title on the console line.
+            Console.WriteLine(GetActiveWindowTitle());
         }
 
         private String watchingTitle = "Signal";
